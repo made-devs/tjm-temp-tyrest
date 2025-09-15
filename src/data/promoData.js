a0c6c0b761250d47139b3f7bbfd09e5c0171c680
made-devs <nmaderama@gmail.com>
2025-09-15 10:32:12 +0700

    Add Promo September

--- src/data/promoData.js
+++ src/data/promoData.js
@@ -1,23 +1,23 @@
 export const promoData = [
   {
-    title: 'Promo Kemerdekaan HUT RI',
+    title: "PROMO SPEKTA SEPTEMBER EKSTRA",
     description:
-      'Dapatkan 21 promo gratis senilai 4,1 juta rupiah. Promo terbatas khusus bulan ini!',
-    thumbnail: '/promo/promo1.webp',
-    videoUrl: 'https://www.instagram.com/p/DMzxjnwxO3C/', // URL diperbarui
+      "Dapatkan 25 promo gratis senilai total Rp 5 juta! Nikmati berbagai layanan perawatan mobil lengkap, mulai dari diskon 50% Nano Ceramic Coating, gratis inspeksi 65 titik, hingga tebus murah Service AC Rp 197 ribu.",
+    thumbnail: "/promo/promo4.webp",
+    videoUrl: "https://www.instagram.com/p/DOGnd0REebL/", // URL diperbarui
   },
   {
-    title: '19 Promo Service AC Gratis',
+    title: "19 Promo Service AC Gratis",
     description:
-      'Nikmati 19 promo gratis service AC senilai 3.7jt, termasuk Diskon Coating, Free Spooring, Tune-Up, dan banyak lagi!',
-    thumbnail: '/promo/promo2.webp',
-    videoUrl: 'https://www.instagram.com/reel/DMx4TdMyeFm/', // URL diperbarui
+      "Nikmati 19 promo gratis service AC senilai 3.7jt, termasuk Diskon Coating, Free Spooring, Tune-Up, dan banyak lagi!",
+    thumbnail: "/promo/promo5.webp",
+    videoUrl: "https://www.instagram.com/p/DOfop7ykebI/", // URL diperbarui
   },
   {
-    title: 'Promo Kaki-Kaki Terbesar',
+    title: "Promo Kaki-Kaki Terbesar",
     description:
-      'Promo kemerdekaan terbesar! Dapatkan bagian dari total hadiah senilai 80,8jt rupiah untuk servis kaki-kaki.',
-    thumbnail: '/promo/promo3.webp',
-    videoUrl: 'https://www.instagram.com/reel/DMx1_qSyS6T/', // URL diperbarui
+      "Promo kemerdekaan terbesar! Dapatkan bagian dari total hadiah senilai 80,8jt rupiah untuk servis kaki-kaki.",
+    thumbnail: "/promo/promo3.webp",
+    videoUrl: "https://www.instagram.com/reel/DMx1_qSyS6T/", // URL diperbarui
   },
 ];
