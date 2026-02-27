74626d27a47f799eec6495c2bb3c5a1119d41b5a
made-devs <nmaderama@gmail.com>
2026-02-27 16:40:38 +0700

    Pasang Google Analytic - Fix Build

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -2,22 +2,22 @@
 
 import { useEffect } from "react";
 import Script from "next/script";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function GoogleAnalytics({ measurementId }) {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     if (!measurementId || !pathname) return;
 
-    const queryString = searchParams?.toString();
-    const url = queryString ? `${pathname}?${queryString}` : pathname;
+    const queryString =
+      typeof window !== "undefined" ? window.location.search : "";
+    const url = `${pathname}${queryString}`;
 
     window.gtag?.("config", measurementId, {
       page_path: url,
     });
-  }, [measurementId, pathname, searchParams]);
+  }, [measurementId, pathname]);
 
   if (!measurementId) return null;
 
